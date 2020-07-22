09b6c884ae6e198ec73ce3960e7fc01787c8f908
rohit-mehra <contactrohitmehra@gmail.com>
2020-07-22 17:03:25 -0500

    added data package and pd_ops module

--- src/boondh/__init__.py
+++ src/boondh/__init__.py
@@ -4,3 +4,4 @@ __version__ = "0.1"
 from .utils.timing import timer
 from .utils.parallel import mp_func
 from .utils.save_load import (save_json, load_json, load_pickle, save_pickle, assert_file_exists, assert_dir_exists)
+from .data.pd_ops import (assert_column_in_dataframe, df_get_dummy, df_filter_values_above_freq)
---
+++ src/boondh/data/pd_ops.py
@@ -0,0 +1,51 @@
+from pandas import DataFrame
+from string import ascii_uppercase
+from random import randint
+
+
+def df_filter_values_above_freq(df: DataFrame, column_name: str, freq: int) -> DataFrame:
+    """Filters data frame on a column's value counts.
+
+    Args:
+        df (DataFrame): DataFrame to be filtered
+        column_name (str): target column
+        freq (int): minimum freq (inclusive) of values in that column to be accepted in new dataframe
+
+    Returns:
+        DataFrame: DataFrame with only those rows which have values that occur in that column more than `freq` times
+    """
+    assert_column_in_dataframe(column_name, df)
+    value_counts = df[column_name].value_counts()
+    values_with_freq_above = value_counts.loc[value_counts >= freq].index.values
+    return df.loc[df[column_name].isin(values_with_freq_above)]
+
+
+def df_get_dummy() -> DataFrame:
+    """Create a dummy dataframe for experiments..
+
+    Returns:
+        DataFrame: DataFrame of shape (26, 3)
+    """
+    sales = [randint(0, 100) for _ in range(len(ascii_uppercase))]
+    category = [sale // 10 for sale in sales]
+    data = DataFrame(
+        {"name": list(ascii_uppercase), "sales": sales, "category": category,}
+    )
+    return data.sample(data.shape[0]).reset_index(drop=True)
+
+
+def assert_column_in_dataframe(column_name: str, df: DataFrame):
+    """Assert column in a dataframe
+
+    Args:
+        column_name (str): desired column
+        df (DataFrame): target dataframe
+    """
+    assert (
+        column_name in df.columns.values
+    ), f"{column_name} not in DataFrame\nColumns={df.columns.values.tolist()}"
+
+
+if __name__ == "__main__":
+    df = df_get_dummy()
+    print(df_filter_values_above_freq(df, "category", 5))
